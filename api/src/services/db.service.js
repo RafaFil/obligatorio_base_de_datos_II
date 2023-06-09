662fb7414942826812e8116b454f13b22c1c0129
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-06-09 19:24:36 -0300

    added first connection and package

---
+++ api/src/services/db.service.js
@@ -0,0 +1,17 @@
+import { Pool } from 'pg'
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'client',
+    password: 'user',
+    database: 'suppBuddyDB',
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+
+
+module.exports = {
+    pool
+}
